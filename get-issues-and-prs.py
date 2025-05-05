804dab80791b16a0b4832bc9a89e245e6ee301a5
Érico Nogueira <erico.rolim@lnls.br>
2025-05-05 13:13:48 -0300

    Don't hardcode list of repositories.
    7 repositories were missing from the list. Instead of adding them
    manually, refactor the script to automatically fetch the list of
    repositories from the organization, and add a sort_repos function to
    keep the areaDetector, ADCore and ADSupport output on top, since those
    are the main repositories.

--- get-issues-and-prs.py
+++ get-issues-and-prs.py
@@ -6,19 +6,13 @@ import subprocess
 from datetime import datetime, timedelta
 
 DEFAULT_OWNER = 'areaDetector'
-DEFAULT_REPOS = [
-    'areaDetector', 'ADCore', 'ADEiger', 'ADAravis', 'ADGenICam',
-    'ADSpinnaker', 'ADVimba', 'ADAndor3', 'ADAndor', 'ADSimDetector',
-    'specsAnalyser', 'ADPylon', 'pvaDriver', 'ADPvCam', 'ADSupport', 'ADUVC',
-    'ADBitFlow', 'ADLambda', 'ADProsilica', 'ADPluginBar', 'ffmpegServer',
-    'ADCompVision', 'ADViewers', 'ADQImaging', 'ADPICam', 'ADCSimDetector',
-    'NDDriverStdArrays', 'ADSBIG', 'ADPSL', 'ADSBIG', 'ADPerkinElmer',
-    'ADLightField', 'ADFireWireWin', 'ADDexela', 'ADPilatus', 'ADMythen',
-    'ADmar345', 'ADRIXSCam', 'ADURL', 'ADmarCCD', 'ADPointGrey', 'ADFastCCD',
-    'ADPixirad', 'ADPcoWin', 'ADPhotron', 'ADPluginCentroids', 'ADADSC',
-    'ADBruker', 'ADPhotonII', 'ADRoper', 'ADnED', 'ADPluginEdge', 'ADPCO',
-    'ADMerlin', 'ADMMPAD', 'ADCameralink', 'ADTimePix', 'firewireDCAM',
-    'ADBinaries', 'ffmpegViewer', 'ADExample'
+DEFAULT_SKIP_REPOS = [
+    'ADKinetix', # has disabled issues
+]
+DEFAULT_FIRST_REPOS = [
+    'areaDetector',
+    'ADCore',
+    'ADSupport',
 ]
 DEFAULT_MAX_DAYS_OLD = 31
 
@@ -26,7 +20,10 @@ DEFAULT_MAX_DAYS_OLD = 31
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--owner', default=DEFAULT_OWNER)
-    parser.add_argument('--repos', nargs='+', default=DEFAULT_REPOS)
+    parser.add_argument('--repos', nargs='+', default=None)
+    parser.add_argument('--skip-repos', nargs='+', default=DEFAULT_SKIP_REPOS)
+    parser.add_argument('--first-repos', nargs='+', default=DEFAULT_FIRST_REPOS)
+    parser.add_argument('--no-skip-archived', action='store_false', dest='skip_archived')
     parser.add_argument('--days', type=int, default=DEFAULT_MAX_DAYS_OLD)
     return parser.parse_args()
 
@@ -39,6 +36,30 @@ def filter_old(items, days):
     now = datetime.now()
     return [item for item in items
             if now - parse_time(item['updatedAt']) <= timedelta(days=days)]
+
+
+def get_repos(owner, skip_archived):
+    command = ['gh', 'repo', 'list', '--limit', '1000', '--json', 'name']
+    if skip_archived:
+        command += ['--no-archived']
+    command += [owner]
+    output = subprocess.check_output(command)
+    repos = json.loads(output)
+    return [repo['name'] for repo in repos]
+
+
+def sort_repos(repos, first_repos):
+    repos_set = set(repos)
+    first_repos_set = set(first_repos)
+
+    # sort repositories after first_repos
+    repos = list(repos_set - first_repos_set)
+    repos.sort()
+
+    # only include first_repos that were in the main list
+    first_repos = [repo for repo in first_repos if repo in repos_set]
+
+    return first_repos + repos
 
 
 def get_issues(project):
@@ -57,7 +78,16 @@ def get_prs(project):
 
 def main():
     args = parse_args()
-    for repo in args.repos:
+    if args.repos is None:
+        repos = get_repos(args.owner, skip_archived=args.skip_archived)
+    else:
+        repos = args.repos
+
+    repos = sort_repos(repos, args.first_repos)
+
+    for repo in repos:
+        if repo in args.skip_repos:
+            continue
         project = f'{args.owner}/{repo}'
         issues = get_issues(project)
         issues = filter_old(issues, args.days)
